d42c288018853a484a4aa41b947669ddc3ea79c2
Josh O'Rourke <josh.orourke@kdmc.net>
2016-09-23 14:56:58 -0400

    Add user-friendly import statements

---
+++ hcpytools/__init__.py
@@ -0,0 +1,7 @@
+from .deploy_supervised_model import DeploySupervisedModel
+from .develop_supervised_model import DevelopSupervisedModel
+
+__all__ = [
+    'DeploySupervisedModel',
+    'DevelopSupervisedModel'
+]
