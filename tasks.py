be9b9706a80a87667639ef63049f08fc8bd35c19
Faris Chebib <faris@zaneprep.com>
2016-10-19 16:42:55 -0600

    Cleanup of setup.py and documentation adding.
    Added nose test references, invoke scripts, dev-requirements, and
    AUTHORS file. Also converted README to rst and removed scripts
    references from the setup.py as we don't want to add them to the $PATH
    upon installation.
    Next steps are to write some more docs for installation and testing as
    well as re-packaging the csv and example files more cleanly.

---
+++ tasks.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from invoke import task
+
+import os
+import webbrowser
+
+REL = os.path.dirname(os.path.realpath(__file__))
+
+
+@task
+def docs(ctx):
+    os.chdir("docs")
+    ctx.run("make html")
+    os.chdir(REL)
+    ctx.run("sphinx-autobuild docs docs/_build/html/ -p 8001")
+    webbrowser.open("http://127.0.0.1:8001")
