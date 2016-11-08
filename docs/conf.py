e83ea5c19df14b24d23fab5c6ca9a591e9bbe7a5
Levi Thatcher <levi.thatcher@healthcatalyst.com>
2016-11-08 13:40:53 -0700

    Adding Cory's css help

--- docs/conf.py
+++ docs/conf.py
@@ -160,7 +160,9 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_context = {
+    'css_files': ['_static/css/hcstyle.css','http://fonts.googleapis.com/css?family=Open+Sans'],
+}
 # Adding custom theme
 #html_style = 'css/my_theme.css'
 
@@ -345,3 +347,5 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #
 # texinfo_no_detailmenu = False
+def setup(app):
+    app.add_stylesheet('css/hcstyle.css')
