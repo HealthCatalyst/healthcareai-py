abe3d4e1fcbc55162a7b1c89f267b01d1531fb40
mmastand <michael.mastanduno@healthcatalyst.com>
2017-03-03 14:06:52 -0700

    corrected unit tests.

--- healthcareai/tests/test_model_eval.py
+++ healthcareai/tests/test_model_eval.py
@@ -3,37 +3,39 @@ import pandas as pd
 import numpy as np
 import unittest
 
-class Test_AUC(unittest.TestCase):
-
+class TestROC(unittest.TestCase):
     def setUp(self):
         self.df = pd.DataFrame({'a':np.repeat(np.arange(.1,1.1,.1),10)})
         b = np.repeat(0,100)
         b[[56,62,63,68,74,75,76,81,82,84,85,87,88] + list(range(90,100))] = 1
         self.df['b'] = b
 
-    def runTestROC_AUC(self):
-        out = GenerateAUC(self['a'],self['b'],'SS',False,False)
-        self.assertAlmostEqual(out['AU_ROC'], 0.94)
-
-    def runTestPR_AUC(self):
-        out = GenerateAUC(self['a'],self['b'],'PR',False,False)
-        self.assertAlmostEqual(out['AU_PR'], 0.86)
-
-    def runTestROC_tpr(self):
-        out = GenerateAUC(self['a'],self['b'],'SS',False,False)
-        self.assertAlmostEqual(out['BestTpr'], 0.9565)
-
-    def runTestROC_fpr(self):
-        out = GenerateAUC(self['a'],self['b'],'SS',False,False)
-        self.assertAlmostEqual(out['BestFpr'], 0.2338)
-
-    def runTestPR_precision(self):
-        out = GenerateAUC(self['a'],self['b'],'PR',False,False)
-        self.assertAlmostEqual(out['BestPrecision'], 0.8)
-
-    def runTestPR_recall(self):
-        out = GenerateAUC(self['a'], self['b'], 'PR', False, False)
-        self.assertAlmostEqual(out['BestRecall'], 0.6956)
+    def runTest(self):
+        # ROC_AUC
+        out = GenerateAUC(self.df['a'],self.df['b'],'SS',False,False)
+        self.assertAlmostEqual(round(out['AU_ROC'],4), 0.9433)
+        self.assertAlmostEqual(round(out['BestTpr'],4), 0.9565)
+        self.assertAlmostEqual(round(out['BestFpr'],4), 0.2338)
 
     def tearDown(self):
         del self.df
+
+class TestPR(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame({'a':np.repeat(np.arange(.1,1.1,.1),10)})
+        b = np.repeat(0,100)
+        b[[56,62,63,68,74,75,76,81,82,84,85,87,88] + list(range(90,100))] = 1
+        self.df['b'] = b
+
+    def runTest(self):
+        # PR_AUC
+        out = GenerateAUC(self.df['a'],self.df['b'],'PR',False,False)
+        self.assertAlmostEqual(round(out['AU_PR'],4), 0.8622)
+        self.assertAlmostEqual(round(out['BestPrecision'], 4), 0.8000)
+        self.assertAlmostEqual(round(out['BestRecall'], 4), 0.6957)
+
+    def tearDown(self):
+        del self.df
+
+if __name__ == '__main__':
+    unittest.main()
