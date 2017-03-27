82766192c001e6e8ec0888c85aeec9b7e5945123
Aaron Neiderhiser <aaron.neiderhiser@healthcatalyst.com>
2017-03-27 17:13:47 -0600

    impact code test

--- healthcareai/tests/test_impact_coding.py
+++ healthcareai/tests/test_impact_coding.py
@@ -9,98 +9,32 @@ from healthcareai.common.impact_coding import impact_coding_on_a_single_column
 
 
 class TestImpactCoding(unittest.TestCase):
-    def test_1(self):
-        df = pd.read_csv(fixture('iris_classification.csv'),
-                         na_values=['None'])
+    ''' Tests :
+        a) column is being re-named correctly
+        b) column is dropped
+        c) number of impact values equals the number of distinct categories
+        d) amount of data after is equal to the expected number of rows
+        e) actual values are assigned correctly
+    '''     
 
+    def test_column_renaming(self):
+        df = pd.read_csv(fixture('iris_classification.csv'),na_values=['None'])
         test_df = impact_coding_on_a_single_column(df, 'species','DRG')
-
-        ''' Tests :
-            a) column is being re-named correctly
-            b) column is dropped
-            c) number of impact values equals the number of distinct categories
-            d) amount of data after is equal to the expected number of rows
-            e) actual values are assigned correctly
-        '''   
-
+        
         self.assertEqual(
-                df['DRG'].name + "_impact_coded", "DRG_impact_coded"                
+                df['DRG'].name + "_impact_coded", "DRG_impact_coded" 
                 )
         
-class TestRFDevTuneTrueRegular(unittest.TestCase):
-    def setUp(self):
-        df = pd.read_csv(fixture('HCPyDiabetesClinical.csv'),
-                         na_values=['None'])
-
-        # Drop uninformative columns
-        df.drop(['PatientID', 'InTestWindowFLG'], axis=1, inplace=True)
-
-        np.random.seed(42)
-        self.o = DevelopSupervisedModel(dataframe=df, model_type='classification',
-                                        predicted_column='ThirtyDayReadmitFLG')
-        self.o.data_preparation(impute=True)
-
-        self.o.random_forest(cores=1, tune=True)
-
-    def runTest(self):
-
-        self.assertAlmostEqual(np.round(self.o.au_roc, 6), 0.968028)
-
-    def tearDown(self):
-        del self.o
-
-
-class TestRFDevTuneTrue2ColError(unittest.TestCase):
-    def setUp(self):
-        cols = ['ThirtyDayReadmitFLG', 'SystolicBPNBR', 'LDLNBR']
-        df = pd.read_csv(fixture('HCPyDiabetesClinical.csv'),
-                         na_values=['None'],
-                         usecols=cols)
-
-        np.random.seed(42)
-        self.o = DevelopSupervisedModel(dataframe=df, model_type='classification',
-                                        predicted_column='ThirtyDayReadmitFLG')
-        self.o.data_preparation(impute=True)
-
-    def runTest(self):
-        self.assertRaises(ValueError, lambda: self.o.random_forest(cores=1,
-                                                                   tune=True))
-
-    def tearDown(self):
-        del self.o
-
-
-class TestLinearDevTuneFalse(unittest.TestCase):
-    def setUp(self):
-        df = pd.read_csv(fixture('HCPyDiabetesClinical.csv'), na_values=['None'])
-
-        # Drop uninformative columns
-        df.drop(['PatientID', 'InTestWindowFLG'], axis=1, inplace=True)
-
-        np.random.seed(42)
-        self.o = DevelopSupervisedModel(dataframe=df, model_type='classification',
-                                        predicted_column='ThirtyDayReadmitFLG')
-        self.o.data_preparation(impute=True)
-        self.o.linear(cores=1)
-
-    def runTest(self):
-        self.assertAlmostEqual(np.round(self.o.au_roc, 3), 0.672000)
-
-    def tearDown(self):
-        del self.o
-
-
-class TestHelpers(unittest.TestCase):
-    def test_class_counter_on_binary(self):
-        df = pd.read_csv(fixture('HCPyDiabetesClinical.csv'), na_values=['None'])
-        df.dropna(axis=0, how='any', inplace=True)
-        result = count_unique_elements_in_column(df, 'ThirtyDayReadmitFLG')
-        self.assertEqual(result, 2)
-
-    def test_class_counter_on_many(self):
-        df = pd.read_csv(fixture('HCPyDiabetesClinical.csv'), na_values=['None'])
-        result = count_unique_elements_in_column(df, 'PatientEncounterID')
-        self.assertEqual(result, 1000)
-
+    def test_unique_values(self):
+        df = pd.read_csv(fixture('iris_classification.csv'),na_values=['None'])
+        unique_drgs = len(df.DRG.unique())
+        test_df = impact_coding_on_a_single_column(df, 'species','DRG')
+        unique_impact_values = len(test_df.DRG_impact_coded.unique())
+      
+        self.assertLessEqual(
+                unique_impact_values, unique_drgs
+                )        
+        
 if __name__ == '__main__':
     unittest.main()
+
