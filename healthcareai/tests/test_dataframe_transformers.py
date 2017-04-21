fb5786e1b2d173ccb5937169e240910e04aff810
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-04-21 07:39:15 -0600

    * tests for DataFrameConvertTargetToBinary

--- healthcareai/tests/test_dataframe_transformers.py
+++ healthcareai/tests/test_dataframe_transformers.py
@@ -48,6 +48,7 @@ class TestDataframeImputer(unittest.TestCase):
         self.assertFalse(result.isnull().values.any())
         # Assert column types remain identical
         self.assertTrue(list(result.dtypes) == list(df.dtypes))
+        self.assertTrue(expected.equals(result))
 
     def test_imputation_for_mean_of_numeric_and_mode_for_categorical(self):
         df = pd.DataFrame([
@@ -72,8 +73,38 @@ class TestDataframeImputer(unittest.TestCase):
         # Assert column types remain identical
         self.assertTrue(list(result.dtypes) == list(df.dtypes))
 
-        # Assert column types remain identical
-        self.assertTrue(list(result.dtypes) == list(df.dtypes))
+
+class TestDataFrameConvertTargetToBinary(unittest.TestCase):
+    def test_does_nothing_on_regression(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'outcome': [1, 5, 4],
+            'string_outcome': ['Y', 'N', 'Y']
+        })
+
+        result = DataFrameConvertTargetToBinary('regression', 'string_outcome').fit_transform(df)
+
+        self.assertTrue(df.equals(result))
+
+    def test_converts_y_n_for_classification(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'outcome': [1, 5, 4],
+            'string_outcome': ['Y', 'N', 'Y']
+        })
+
+        expected = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'outcome': [1, 5, 4],
+            'string_outcome': [1, 0, 1]
+        })
+
+        result = DataFrameConvertTargetToBinary('classification', 'string_outcome').fit_transform(df)
+
+        self.assertTrue(expected.equals(result))
 
 
 if __name__ == '__main__':
