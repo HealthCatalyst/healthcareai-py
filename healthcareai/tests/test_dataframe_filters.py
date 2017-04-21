17ac7327c89d55185b6c7a03c052947d105ff784
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-04-20 19:10:13 -0600

    * fixed and augmented dataframe filters tests * fixed a bug in TestDataframeGrainColumnDataFilter test * tested helper functions for dataframe filters * new tests for DataframeNullValueFilter

--- healthcareai/tests/test_dataframe_filters.py
+++ healthcareai/tests/test_dataframe_filters.py
@@ -1,7 +1,53 @@
 import pandas as pd
 import unittest
-from healthcareai.common.filters import DataframeDateTimeColumnSuffixFilter, DataframeGrainColumnDataFilter, DataframeNullValueFilter
+from healthcareai.common.filters import *
 from healthcareai.common.healthcareai_error import HealthcareAIError
+
+
+class TestIsDataframe(unittest.TestCase):
+    def test_is_dataframe(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'age': [1, 5, 4]
+        })
+
+        self.assertTrue(is_dataframe(df))
+
+    def test_is_not_dataframe(self):
+        junk_inputs = [
+            'foo',
+            42,
+            [1, 2, 3],
+            [[1, 2, 3], [1, 2, 3], [1, 2, 3], ],
+            {'a': 1}
+        ]
+
+        for junk in junk_inputs:
+            self.assertFalse(is_dataframe(junk))
+
+
+class TestValidationError(unittest.TestCase):
+    def test_is_dataframe(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'age': [1, 5, 4]
+        })
+
+        self.assertIsNone(validate_dataframe_input(df))
+
+    def test_is_not_dataframe(self):
+        junk_inputs = [
+            'foo',
+            42,
+            [1, 2, 3],
+            [[1, 2, 3], [1, 2, 3], [1, 2, 3], ],
+            {'a': 1}
+        ]
+
+        for junk in junk_inputs:
+            self.assertRaises(HealthcareAIError, validate_dataframe_input, junk)
 
 
 class TestDataframeColumnSuffixFilter(unittest.TestCase):
@@ -69,7 +115,7 @@ class TestDataframeColumnSuffixFilter(unittest.TestCase):
         self.assertEqual(list(result.columns).sort(), expected.sort())
 
 
-class DataframeGrainColumnDataFilter(unittest.TestCase):
+class TestDataframeGrainColumnDataFilter(unittest.TestCase):
     def test_raises_error_on_non_dataframe_inputs(self):
         junk_inputs = [
             'foo',
@@ -80,7 +126,7 @@ class DataframeGrainColumnDataFilter(unittest.TestCase):
         ]
 
         for junk in junk_inputs:
-            self.assertRaises(HealthcareAIError, DataframeGrainColumnDataFilter().fit_transform, junk)
+            self.assertRaises(HealthcareAIError, DataframeGrainColumnDataFilter(None).fit_transform, junk)
 
     def test_removes_nothing_when_it_finds_no_matches(self):
         df = pd.DataFrame({
@@ -89,32 +135,19 @@ class DataframeGrainColumnDataFilter(unittest.TestCase):
             'age': [1, 5, 4]
         })
 
-        result = DataframeGrainColumnDataFilter().fit_transform(df)
+        result = DataframeGrainColumnDataFilter('PatientID').fit_transform(df)
 
         self.assertEqual(len(result), 3)
         self.assertEqual(list(result.columns).sort(), list(df.columns).sort())
 
-    def test_removes_three_character_match(self):
+    def test_removes_match(self):
         df = pd.DataFrame({
             'category': ['a', 'b', 'c'],
             'gender': ['F', 'M', 'F'],
-            'DTS': [1, 5, 4]
+            'PatientID': [1, 5, 4]
         })
 
-        result = DataframeGrainColumnDataFilter().fit_transform(df)
-        expected = ['category', 'gender']
-
-        self.assertEqual(len(result), 3)
-        self.assertEqual(list(result.columns).sort(), expected.sort())
-
-    def test_removes_long_character_match(self):
-        df = pd.DataFrame({
-            'category': ['a', 'b', 'c'],
-            'gender': ['F', 'M', 'F'],
-            'admit_DTS': [1, 5, 4]
-        })
-
-        result = DataframeGrainColumnDataFilter().fit_transform(df)
+        result = DataframeGrainColumnDataFilter('PatientID').fit_transform(df)
         expected = ['category', 'gender']
 
         self.assertEqual(len(result), 3)
@@ -124,18 +157,17 @@ class DataframeGrainColumnDataFilter(unittest.TestCase):
         df = pd.DataFrame({
             'category': ['a', 'b', 'c'],
             'gender': ['F', 'M', 'F'],
-            'admit_dts': [1, 5, 4]
+            'patientid': [1, 5, 4]
         })
 
-        result = DataframeGrainColumnDataFilter().fit_transform(df)
-        expected = ['category', 'gender', 'admit_dts']
+        result = DataframeGrainColumnDataFilter('PatientID').fit_transform(df)
+        expected = ['category', 'gender', 'patientid']
 
         self.assertEqual(len(result), 3)
         self.assertEqual(list(result.columns).sort(), expected.sort())
 
 
-class DataframeNullValueFilter(unittest.TestCase):
-    # TODO fill out with more tests
+class TestDataframeNullValueFilter(unittest.TestCase):
     def test_raises_error_on_non_dataframe_inputs(self):
         junk_inputs = [
             'foo',
@@ -146,7 +178,37 @@ class DataframeNullValueFilter(unittest.TestCase):
         ]
 
         for junk in junk_inputs:
-            self.assertRaises(HealthcareAIError, DataframeNullValueFilter().fit_transform, junk)
+            self.assertRaises(HealthcareAIError, DataframeGrainColumnDataFilter(None).fit_transform, junk)
+
+    def test_removes_nothing_when_no_nulls_exist(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'age': [1, 5, 4]
+        })
+
+        result = DataframeNullValueFilter().fit_transform(df)
+        self.assertEqual(len(result), 3)
+
+    def test_removes_row_with_single_null(self):
+        df = pd.DataFrame({
+            'category': ['a', 'b', 'c'],
+            'gender': ['F', 'M', 'F'],
+            'age': [1, 5, None]
+        })
+
+        result = DataframeNullValueFilter().fit_transform(df)
+        self.assertEqual(len(result), 2)
+
+    def test_removes_row_with_all_nulls(self):
+        df = pd.DataFrame({
+            'category': ['a', None, None],
+            'gender': ['F', 'M', None],
+            'age': [1, 5, None]
+        })
+
+        result = DataframeNullValueFilter().fit_transform(df)
+        self.assertEqual(len(result), 1)
 
 
 if __name__ == '__main__':
