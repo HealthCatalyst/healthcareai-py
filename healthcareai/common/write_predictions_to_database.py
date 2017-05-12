d1db144349d5200d3d03fad208b24c74e5d1b1c2
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-05-12 17:06:02 -0600

    WIP

--- healthcareai/common/write_predictions_to_database.py
+++ healthcareai/common/write_predictions_to_database.py
@@ -1,7 +1,77 @@
+import sqlalchemy
 import pyodbc
+import pandas as pd
+import sqlite3
+
 import healthcareai.common.database_connection_validation as db_validation
 
-def write_predictions_to_database(server, destination_db_schema_table, predicted_column_name, grain_column, output_2dlist):
+
+def build_mssql_connection_string(server):
+    # https://www.connectionstrings.com/
+    return 'DRIVER={SQL Server Native Client 11.0}; SERVER={}; Trusted_Connection=yes;'.format(server)
+
+
+def build_mysql_connection_string(server, database, userid, password):
+    return 'Server={};Database={};Uid={};Pwd={}; '.format(server, database, userid, password)
+
+
+def build_sqlite_connection_string(file_path):
+    # https://www.connectionstrings.com/
+    return 'Data Source={};Version=3;'.format(file_path)
+
+
+def build_sqlite_in_memory_connection_string():
+    # https://www.connectionstrings.com/
+    return 'Data Source=:memory:;Version=3;New=True;'
+
+
+def write_to_mssql(server, destination_db_schema_table, predicted_column_name, grain_column, output_2dlist):
+    connection_string = "{}".format(server, destination_db_schema_table)
+    dataframe = pd.DataFrame(output_2dlist)
+
+
+def write_to_sqlite(db_filename, predicted_column_name, grain_column, output_2dlist):
+    connection_string = "{}".format()
+    # TODO nuke the 2d list portion - why do this if pandas is the end?
+    dataframe = pd.DataFrame(output_2dlist)
+
+    try:
+        pass
+    except error
+        raise
+
+
+def write_to_mysql(server, destination_db_schema_table, predicted_column_name, grain_column, output_2dlist):
+    connection_string = "{}".format(server, destination_db_schema_table)
+    # TODO nuke the 2d list portion - why do this if pandas is the end?
+    dataframe = pd.DataFrame(output_2dlist)
+
+
+def db_agnostic_writing(connection_string, dataframe):
+    # TODO nuke the 2d list portion - why do this if pandas is the end?
+    # TODO Validate inputs
+    try:
+        engine = sqlalchemy.create_engine(connection_string)
+
+        dataframe.to_sql(engine)
+
+        # Todo: count and display (via pyodbc) how many rows inserted
+        print("\nSuccessfully inserted rows into {}.".
+              format(destination_db_schema_table))
+
+    # TODO need to find a good list of errors to catch here and how to test them
+    except RuntimeError:
+        print("\nFailed to insert values into {}.".format(destination_db_schema_table))
+        print("Was your test insert successful earlier?")
+        print("If so, what has changed with your entity since then?")
+
+        # TODO make another method that takes a connection string
+        db_validation.validate_destination_table_connection(server, destination_db_schema_table, grain_column,
+                                                            predicted_column_name)
+
+
+def write_predictions_to_database(server, destination_db_schema_table, predicted_column_name, grain_column,
+                                  output_2dlist):
     db_connection = pyodbc.connect("""DRIVER={SQL Server Native Client 11.0};
                                SERVER=""" + server + """;
                                Trusted_Connection=yes;""")
@@ -24,7 +94,8 @@ def write_predictions_to_database(server, destination_db_schema_table, predicted
         print("Was your test insert successful earlier?")
         print("If so, what has changed with your entity since then?")
 
-        db_validation.validate_destination_table_connection(server, destination_db_schema_table, grain_column, predicted_column_name)
+        db_validation.validate_destination_table_connection(server, destination_db_schema_table, grain_column,
+                                                            predicted_column_name)
 
     finally:
         try:
