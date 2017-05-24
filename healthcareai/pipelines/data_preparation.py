5d63965bc6123445a99b1edb00723c6d9a2283a2
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-05-24 16:36:05 -0600

    * note about an issue in data_prep * deprecated old code in data_prep * first pass mermaid.js diagram

--- healthcareai/pipelines/data_preparation.py
+++ healthcareai/pipelines/data_preparation.py
@@ -12,7 +12,8 @@ def full_pipeline(model_type, predicted_column, grain_column, impute=True):
         ('remove_grain_column', filters.DataframeColumnRemover(grain_column)),
         # Perform one of two basic imputation methods
         # TODO we need to think about making this optional to solve the problem of rare and very predictive values
-        #   where neither imputation or dropping rows is appropriate
+        # TODO This pipeline may drop nulls in prediction rows if impute=False
+        # TODO See https://github.com/HealthCatalyst/healthcareai-py/issues/276
         ('imputation', transformers.DataFrameImputer(impute=impute)),
         ('null_row_filter', filters.DataframeNullValueFilter(excluded_columns=None)),
         ('convert_target_to_binary', transformers.DataFrameConvertTargetToBinary(model_type, predicted_column)),
@@ -20,21 +21,3 @@ def full_pipeline(model_type, predicted_column, grain_column, impute=True):
         ('create_dummy_variables', transformers.DataFrameCreateDummyVariables([predicted_column])),
     ])
     return pipeline
-
-
-def dataframe_prediction(dataframe, model_type, grain_column_name, predicted_column, impute=True):
-    # TODO Deprecate this
-    """
-    Main prediction data preparation pipeline. Sequentially runs transformers and methods to clean and prepare the
-    before dropping the prediction column
-    """
-
-    # Apply the pipelines
-    # TODO do we want to enforce imputation so that entire rows with null values don't get dropped?
-    # TODO ... or do we want to leave out the null dropping step - and if so, what impact will this have ML-wise?
-    result_dataframe = full_pipeline(model_type, predicted_column, grain_column_name, impute=impute).transform(dataframe)
-
-    # Remove the predicted column
-    result_dataframe = filters.DataframeColumnRemover(predicted_column).fit_transform(result_dataframe)
-
-    return result_dataframe
