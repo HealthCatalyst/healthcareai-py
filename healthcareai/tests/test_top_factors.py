1bd06851879a5bbb58f674df12be25ca69f81aa6
Taylor Miller <taylor.miller@healthcatalyst.com>
2017-06-19 09:33:31 -0600

    * minor formatting tweaks * minor comment update

--- healthcareai/tests/test_top_factors.py
+++ healthcareai/tests/test_top_factors.py
@@ -11,7 +11,7 @@ class TestTopFactors(unittest.TestCase):
     def setUpClass(cls):
         """ Load a dataframe, train a linear model and prepare a prediction dataframe for assertions """
 
-        # Build a dataframe
+        # Build a toy dataset with known correlations (and some noise) for testing
         # TODO Modify probs and cat_weights (and expected df) once top factors is made independent of dummification
         probs = (0.334, 0.333, 0.333)  # distribution of categorical
         cat_weights = (-3, -4, 7)
@@ -22,14 +22,14 @@ class TestTopFactors(unittest.TestCase):
         rows = 200
         # Set seed to guarantee data set is always the same
         np.random.seed(1066)
-        factors_df = pd.DataFrame({'id': range(rows),  # grain col
-                                   'positive_corr': np.random.normal(size=rows),
-                                   'categorical': np.random.choice(['Common', 'Medium', 'Rare'], p=probs, size=rows),
-                                   'negative_corr': np.random.normal(loc=mu, scale=sigma, size=rows),
-                                   'useless_pred_1': np.random.normal(size=rows),
-                                   'useless_pred_2': np.random.choice(['Y', 'N'], size=rows)},
-                                  columns=['id', 'positive_corr', 'categorical', 'negative_corr', 'useless_pred_1',
-                                           'useless_pred_2'])
+        factors_df = pd.DataFrame({
+            'id': range(rows),  # grain col
+            'positive_corr': np.random.normal(size=rows),
+            'categorical': np.random.choice(['Common', 'Medium', 'Rare'], p=probs, size=rows),
+            'negative_corr': np.random.normal(loc=mu, scale=sigma, size=rows),
+            'useless_pred_1': np.random.normal(size=rows),
+            'useless_pred_2': np.random.choice(['Y', 'N'], size=rows)},
+            columns=['id', 'positive_corr', 'categorical', 'negative_corr', 'useless_pred_1', 'useless_pred_2'])
 
         # Set true decision boundary using importance
         factors_df['dot_product'] = 4 * factors_df['positive_corr']
@@ -53,9 +53,9 @@ class TestTopFactors(unittest.TestCase):
         training_df = factors_df.copy()
 
         hcai = SupervisedModelTrainer(
-            training_df,
-            'response',
-            'classification',
+            dataframe=training_df,
+            predicted_column='response',
+            model_type='classification',
             impute=True,
             grain_column='id')
 
@@ -83,18 +83,20 @@ class TestTopFactors(unittest.TestCase):
         self.assertTrue(set(expected) == set(results))
 
     def test_factors_are_correct_on_new_predictions(self):
-        new_data = pd.DataFrame({'id': [1001, 1002, 1003],
-                                 'positive_corr': [-3, -0.45, 2],
-                                 'categorical': ['Common', 'Rare', 'Medium'],
-                                 'negative_corr': [-4, 2, 0],
-                                 'useless_pred_1': [0, 0.01, -0.01],
-                                 'useless_pred_2': ['Y', 'N', 'Y'],
-                                 'response': ['N', 'Y', 'Y']})
+        new_data = pd.DataFrame({
+            'id': [1001, 1002, 1003],
+            'positive_corr': [-3, -0.45, 2],
+            'categorical': ['Common', 'Rare', 'Medium'],
+            'negative_corr': [-4, 2, 0],
+            'useless_pred_1': [0, 0.01, -0.01],
+            'useless_pred_2': ['Y', 'N', 'Y'],
+            'response': ['N', 'Y', 'Y']})
 
-        expected = pd.DataFrame({'id': [1001, 1002, 1003],
-                                 'Factor1TXT': ['positive_corr', 'categorical.Rare', 'positive_corr'],
-                                 'Factor2TXT': ['negative_corr', 'negative_corr', 'categorical.Medium'],
-                                 'Factor3TXT': ['useless_pred_2.Y', 'positive_corr', 'useless_pred_2.Y']})
+        expected = pd.DataFrame({
+            'id': [1001, 1002, 1003],
+            'Factor1TXT': ['positive_corr', 'categorical.Rare', 'positive_corr'],
+            'Factor2TXT': ['negative_corr', 'negative_corr', 'categorical.Medium'],
+            'Factor3TXT': ['useless_pred_2.Y', 'positive_corr', 'useless_pred_2.Y']})
 
         results = self.trained_lr.make_factors(new_data)
 
