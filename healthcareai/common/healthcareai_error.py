243365145abd14113d7a3299582bb23b864620ad
Taylor Miller <taylor.miller@hc2191.local>
2017-08-29 09:53:06 -0600

    * corrected tab error

--- healthcareai/common/healthcareai_error.py
+++ healthcareai/common/healthcareai_error.py
@@ -2,12 +2,13 @@
 
 This is the error to communicate things to non technical users.
 """
+
+
 class HealthcareAIError(Exception):
-	
     """This is the error that should be thrown to help communicate problems to users in a nice way."""
 
     def __init__(self, message):
-    	"""Instantiate the error with a given message"""
+        """Instantiate the error with a given message"""
         self.message = message
 
     def __str__(self):
