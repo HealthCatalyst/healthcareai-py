4f101cd9c93b65a06e08f4048fd01bd272e57ee6
mxlei01 <mxlei01@gmail.com>
2017-09-02 15:34:27 -0600

    Merge branch 'master' of https://github.com/HealthCatalyst/healthcareai-py into fix_352

--- healthcareai/common/transformers.py
+++ healthcareai/common/transformers.py
@@ -36,7 +36,7 @@ class DataFrameImputer(TransformerMixin):
 
         self.fill = pd.Series([X[c].value_counts().index[0]
                                if X[c].dtype == np.dtype('O')
-                               or pd.core.common.is_categorical_dtype(X[c])
+                                  or pd.core.common.is_categorical_dtype(X[c])
                                else X[c].mean() for c in X], index=X.columns)
 
         if self.imput_output:
