f18545fd0e97d2b2d8ba55a3a874431b9de47f46
yvanhuele <yannick.vanhuele@healthcatalyst.com>
2017-09-25 11:04:19 -0600

    dealt with mixed types in get_categorical_levels

--- healthcareai/common/get_categorical_levels.py
+++ healthcareai/common/get_categorical_levels.py
@@ -25,9 +25,20 @@ def get_categorical_levels(dataframe, columns_to_ignore):
 
     # Get the distribution of values for each categorical column
     for column in categorical_columns:
+        # Get the counts for each categorical variable
         value_distribution = dataframe[column].value_counts(sort=False)
+
         # Sort by the index to ensure the correct dummy is dropped in get_dummies(drop_first=True)
-        value_distribution.sort_index(inplace=True)  # get counts for each factor level
+        # Do this in several steps to deal with mixed data types (e.g. int and str) in object column
+        # 1. Convert to dataframe
+        value_distribution = value_distribution.to_frame('counts')
+        # 2. Add column consisting of index values as strings
+        value_distribution['str_index'] = value_distribution.index.map(str)
+        # 3. Sort using the index values
+        value_distribution.sort_values('str_index', inplace = True)
+        # 4. Extract the sorted counts as a Series
+        value_distribution = value_distribution['counts']
+
         total_count = value_distribution.values.sum()  # get the number of occurences for all levels of the factor
         # divide the factor level counts by the total number to get the factor level frequencies
         value_distribution *= 1 / total_count
