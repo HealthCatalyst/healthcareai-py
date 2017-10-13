ead031b7b2b3b513a16cf4104aa322dada531e12
mxlei01 <mxlei01@gmail.com>
2017-10-12 21:13:03 -0600

    Changed unit test for training output decorator.

--- healthcareai/tests/test_trainer_decorator.py
+++ healthcareai/tests/test_trainer_decorator.py
@@ -1,12 +1,19 @@
 import unittest
 from healthcareai.supervised_model_trainer import SupervisedModelTrainer
 import healthcareai.datasets as hcai_datasets
+import sys
+from io import StringIO
 
 
 class TestTrainerDecorator(unittest.TestCase):
-    """
+
+    """Tests for the training decorator.
+
+    We will compare a decorated linear regression and a undecorated linear regression output. They should not be
+    the same.
 
     """
+
     def setUp(self):
         df = hcai_datasets.load_diabetes()
 
@@ -14,15 +21,28 @@ class TestTrainerDecorator(unittest.TestCase):
         columns_to_remove = ['PatientID']
         df.drop(columns_to_remove, axis=1, inplace=True)
 
-        self.classification_trainer = SupervisedModelTrainer(dataframe=df,
-                                                             predicted_column='ThirtyDayReadmitFLG',
-                                                             model_type='classification',
-                                                             impute=True,
-                                                             grain_column='PatientEncounterID',
-                                                             verbose=False)
+        self.regression_trainer = SupervisedModelTrainer(df,
+                                                         'SystolicBPNBR',
+                                                         'regression',
+                                                         grain_column='PatientEncounterID',
+                                                         impute=True,
+                                                         verbose=False)
 
-        self.classification_trainer.undecorated_lasso_regression = \
-            lambda myself: myself._advanced_trainer.lasso_regression(randomized_search=False)
+        def undecorated_lr(self):
+            return self._advanced_trainer.linear_regression(randomized_search=False)
+
+        self.regression_trainer.undecorated_lr = undecorated_lr.__get__(self.regression_trainer,
+                                                                        self.regression_trainer.__class__)
 
     def test_decorator(self):
-        model = self.classification_trainer.lasso_regression()
+        out = StringIO()
+        sys.stdout = out
+        self.regression_trainer.linear_regression()
+        decorated_output = out.getvalue().strip()
+
+        out = StringIO()
+        sys.stdout = out
+        self.regression_trainer.undecorated_lr()
+        undecorated_output = out.getvalue().strip()
+
+        assert decorated_output != undecorated_output
