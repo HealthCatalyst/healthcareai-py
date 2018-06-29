6bdbaa821011847203857fce281df037202c3413
Sameer Mahajan <sameer.mahajan@gslab.com>
2018-06-29 10:50:44 +0530

    restore earlier non standard indentation in case it is causing the code-quality/landscape failure

--- healthcareai/common/transformers.py
+++ healthcareai/common/transformers.py
@@ -35,7 +35,7 @@ class DataFrameImputer(TransformerMixin):
 
         self.fill = pd.Series([X[c].value_counts().index[0]
                                if X[c].dtype == np.dtype('O')
-                               or pd.api.types.is_categorical_dtype(X[c])
+                                  or pd.api.types.is_categorical_dtype(X[c])
                                else X[c].mean() for c in X], index=X.columns)
 
         if self.verbose:
